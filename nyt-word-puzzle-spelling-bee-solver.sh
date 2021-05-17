9255a544f9da56af40e98731de036a8545112af2
Todd Hodes <todd@toddh.org>
2021-05-16 17:43:26 -0700

    add script

---
+++ nyt-word-puzzle-spelling-bee-solver.sh
@@ -0,0 +1,34 @@
+#
+# solve NYT "spelling bee" puzzles
+#
+# cat /usr/share/dict/words  |egrep -iv "[bcdehjklmopqrstvxyz]" |egrep [Aa] |sort
+
+case "$1" in
+"") echo "Usage: $0 <letters>"
+    echo "          (required letter first)"
+    exit 1 ;;
+esac
+
+cat <<EOF > /tmp/wpsolver.kts
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val letters = "$1"
+val required = letters[0] + letters[0].toString().toUpperCase()
+var o = ""
+for (i in "abcdefghijklmnopqrstuvwxyz") {
+   if (i !in letters) o += i
+}
+val command = "cat /usr/share/dict/words  |egrep -iv '[" + o + "]' | egrep [" + required + "] | sort"
+System.out.println(command)
+
+val cmd = arrayOf( "/bin/sh", "-c", command)
+val process = Runtime.getRuntime().exec(cmd)
+val reader = BufferedReader(InputStreamReader(process.inputStream))
+val line = reader.readText();
+println(line)
+EOF
+
+kotlinc -script /tmp/wpsolver.kts 2>/dev/null
+
